e2a50245ede612ac1b1d2c8312cf6185540e4894
Boris Musatov <boris.musatov@gmail.com>
2016-12-08 23:38:39 +0100

    Add env vars

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
-docker build . -t apex-lambda-deployer
+docker build . -t apex-lambda-deployer \
+--build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+--build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+--build-arg AWS_REGION=$AWS_REGION
 
 docker run apex-lambda-deployer
